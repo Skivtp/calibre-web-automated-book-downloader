9f9bc928ff99f3196d627a1748673a131246c807
Skivtp <118019606+Skivtp@users.noreply.github.com>
2025-10-27 02:14:32 +0300

    Refactor search and fetch methods in inpx_backend.py

--- inpx_backend.py
+++ inpx_backend.py
@@ -8,21 +8,18 @@ class InpxWebBackend:
         self.ns = {"atom": "http://www.w3.org/2005/Atom"}
 
     def search(self, query):
-        """Ищем книги по названию через OPDS"""
         url = f"{self.base_url}/opds/search?type=title&term={query}"
         r = requests.get(url)
         r.raise_for_status()
         root = ET.fromstring(r.text)
 
         results = []
-        # Первый уровень: подразделы (rel="subsection")
         for entry in root.findall("atom:entry", self.ns):
             subsection = entry.find("atom:link", self.ns).attrib.get("href")
             results.extend(self._fetch_books(subsection))
         return results
 
     def _fetch_books(self, subsection_href):
-        """Заходим в подраздел (title) и достаём список книг"""
         url = urljoin(self.base_url, subsection_href)
         r = requests.get(url)
         r.raise_for_status()
@@ -34,14 +31,12 @@ class InpxWebBackend:
             authors = entry.find("atom:content", self.ns)
             authors = authors.text if authors is not None else "Неизвестен"
 
-            # Здесь ссылки ведут на /opds/book?uid=...
             for link in entry.findall("atom:link", self.ns):
                 if "subsection" in link.attrib.get("rel", ""):
                     books.extend(self._fetch_downloads(link.attrib["href"], title, authors))
         return books
 
     def _fetch_downloads(self, book_href, title, authors):
-        """Заходим в карточку книги и достаём acquisition-ссылки"""
         url = urljoin(self.base_url, book_href)
         try:
             r = requests.get(url)
